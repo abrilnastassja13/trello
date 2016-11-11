6d1f3c1fbb3e6813e4191db1d2f3e3ee0abf6095
abrilnastassja13 <abril.saravia13@gmail.com>
2016-11-11 12:56:11 -0300

    trello

--- js/script.js
+++ js/script.js
@@ -4,16 +4,33 @@ function aparecer() {
 	var cuadroTexto = document.createElement('input');
 	var cajaDos = document.createElement('div');
 	var añadir = document.createTextNode('añadir');
+	var cajaTres = document.createElement('div');
 
 	botoncin.setAttribute('class', 'primer-boton');
 	cuadroTexto.setAttribute('type', 'text');
 	cuadroTexto.setAttribute('placeholder', 'Ingresa tu tarea');
-	cajaDos.setAttribute('id', 'dentro')
+	cuadroTexto.setAttribute('id', 'cuadro')
+	cajaDos.setAttribute('id', 'dentro');
+	cajaTres.setAttribute('id', 'tarea');
 
 	cajaDos.appendChild(botoncin);
 	cajaDos.appendChild(cuadroTexto);
 	caja.appendChild(cajaDos);
 	botoncin.appendChild(añadir);
+	caja.appendChild(cajaTres);
+
+	var escribir = document.getElementsByClassName('primer-boton')[0];
+	escribir.addEventListener('click', function() {
+		var guardar = document.getElementById('cuadro').value;
+		console.log(guardar);
+		var textoTarjeta = document.createTextNode(guardar);
+		var llamarTextoTarjeta = document.getElementById('dentro');
+		var parrafo = document.createElement('span');
+		
+		parrafo.appendChild(textoTarjeta);
+		llamarTextoTarjeta.appendChild(parrafo);
 	
+	});
+
 
 }
